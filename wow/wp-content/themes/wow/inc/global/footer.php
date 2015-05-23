f7e15ab000dfaaf2ffabc1f9537d5eccae138ccc
jtuds <james@jtudsbury.com>
2015-05-23 15:09:34 +0100

    Added wow files

---
+++ wow/wp-content/themes/wow/inc/global/footer.php
@@ -0,0 +1,25 @@
+<footer class="p-footer" role="contentinfo">
+
+	<div class="grid">
+		<div class="grid__item one-half">
+			<div class="p-copyright">Copyright <?php echo date('Y'); ?></div>
+		</div><!--/.grid__item -->
+		<div class="grid__item one-half">
+			<ul class="p-footer-links">
+				<li>Courses</li>
+				<li>About</li>
+				<li>Terms</li>
+			</ul>
+		</div><!--/.grid__item -->
+	</div><!--/.grid -->
+	
+</footer>
+
+<!--[if (gte IE 6)&(lte IE 8)]>
+<script src="//cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
+<![endif]-->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wow/wp-content/themes/wow/inc/global/header.php
@@ -0,0 +1,30 @@
+<body <?php body_class(); ?>>
+
+	<header class="p-topbar" role="banner">
+
+		<div class="wrapper">
+
+			<div class="p-logo">
+				<?php if (is_front_page()) { ?>
+					<img src="http://placehold.it/130x50/9ab384/fff&text=logo">
+					<span class="visuallyhidden">Wow!</span>
+				<?php } else { ?>
+					<a href="<?php bloginfo('url'); ?>">
+						<img src="http://placehold.it/130x50/9ab384/fff&text=logo">
+						<span class="visuallyhidden">Wow!</span>
+					</a>
+				<?php } ?>
+			</div><!--
+
+			--><nav class="p-nav">
+				<ul class="nav">
+					<li><a href="<?php getBasePath(); ?>/?post_type=tribe_events">Events</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Item 1</a></li>
+					<li><a href="#">Item 2</a></li>
+				</ul>
+			</nav>
+
+		</div><!--/.wrapper -->
+
+	</header>
